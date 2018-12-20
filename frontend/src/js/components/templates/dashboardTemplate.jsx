1ce4472c40d4486bf0844f77d6cb0fafdb3d724d
Vitor <vitorbretasprata@gmail.com>
2018-12-19 23:33:54 -0200

    add scrollspy

--- frontend/src/js/components/templates/dashboardTemplate.jsx
+++ frontend/src/js/components/templates/dashboardTemplate.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import Scrollspy from 'react-scrollspy'
+import React, { Component } from 'react'
+import { AnchorLink, AnchorElement, ScrollPanel } from "react-spy-scroll";
+import Axios from 'axios'
 
-const DashboardTemplate = () => (
-    <div className="container-fluid">
-        <div className="row">
-            <section className="col-sm-9">
-            
-            </section>
-            <aside className="col-sm-3">
+const URL = 'http://localhost:2000/api/tickets/getTickets'
 
-            </aside>
-        </div>        
-    </div>
-)
+class DashboardTemplate extends Component {
+    constructor(){
+        super();
+        this.state = {
+            tickets: []
+        }
+    }
+
+
+
+    render(){
+        Axios.get(URL).then((respTickets)=> {
+            this.setState({
+                tickets: respTickets
+            })
+        })
+
+
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <section className="col-sm-9">
+                    <ScrollPanel className="hidden-scrollbar"
+                    style={{ backgroundColor: "white", height: 600, overflow: "scroll" }}
+                    >
+                    <ul>
+                        {this.state.tickets.map((ticket, i) => {
+                          <li key={i}>{ticket.Name}</li>  
+                        })}
+                        
+                    </ul>
+                    </ScrollPanel>
+                    
+                    </section>
+                    <aside className="col-sm-3">
+
+                    </aside>
+                </div>        
+            </div>
+        )
+    }
+}
+
 
 export default DashboardTemplate;

188610cfa2b2d1885b9be63bf62896153929a2fb
Vitor Prata <vitorbretasprata@gmail.com>
2019-07-10 16:45:36 -0300

    changes on home page

--- src/js/components/templates/homeTemplate.jsx
+++ src/js/components/templates/homeTemplate.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CarouselTemplate from '../carouselTemplate';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt, faDonate } from '@fortawesome/free-solid-svg-icons';
 
 const HomeTemplate = () => (
     <div className="homeBody">
@@ -7,11 +9,41 @@ const HomeTemplate = () => (
         <section>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-6">
-                        First Panel
+                    <div className="panels col-sm-12 col-md-6 col-lg-6">
+                        <div>
+                            <FontAwesomeIcon icon={faUserAlt} size="3x"/>
+                        </div>
+                        <div>
+                            <h3>About me</h3>
+                        </div>
+                        <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of type and scrambled it to make a type 
+                            specimen book. It has survived not only five centuries, but also the leap into 
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 
+                            1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+                            recently with desktop publishing software like Aldus PageMaker including versions of 
+                            Lorem Ipsum.
+                        </div>
                     </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6">
-                        Second Panel
+                    <div className="panels col-sm-12 col-md-6 col-lg-6">
+                        <div>
+                            <FontAwesomeIcon icon={faDonate} size="3x"/>
+                        </div>
+                        <div>
+                            <h3>Donation</h3>
+                        </div>
+                        <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of type and scrambled it to make a type 
+                            specimen book. It has survived not only five centuries, but also the leap into 
+                            electronic typesetting, remaining essentially unchanged. It was popularised in the 
+                            1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+                            recently with desktop publishing software like Aldus PageMaker including versions of 
+                            Lorem Ipsum.
+                        </div>
                     </div>
 
                 </div>

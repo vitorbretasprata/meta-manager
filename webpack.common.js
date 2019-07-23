428921bb5404f60acafa3c767cc754693bbc95a0
Vitor Prata <vitorbretasprata@gmail.com>
2019-07-23 16:34:43 -0300

    changes on webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,42 +1,32 @@
 const path = require('path');
-const webpack = require('webpack');
-const dotenv = require('dotenv').config({path: __dirname + '/.env'});
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-source-map',  
-    optimization: {
-        minimizer: [new TerserPlugin()]
-    },  
     resolve: {
         extensions: ['*', '.js', '.jsx'],
         alias: {
             modules: path.resolve(__dirname, 'node_modules')
         }
     },
-    node: {
-        net: 'empty',
-        dns: 'empty'
+    entry: {
+        app: './src/index.jsx'
     },
-    plugins: [ 
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: "./index.html"
-        }),
+    plugins: [
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
         }),
-        new webpack.DefinePlugin({
-            'process.env': JSON.stringify(dotenv.parsed)
+        new HtmlWebpackPlugin({
+            title: 'Production'
         })
     ],
-    devServer: {
-        contentBase: './',
-        historyApiFallback: true
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {        
         rules: [           
@@ -65,4 +55,4 @@ module.exports = {
             }
         ]
     }
-}
+};

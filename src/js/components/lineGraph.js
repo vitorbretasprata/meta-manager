cbb1460d23c26978419abce20e224e467dffd175
Vitor <vitorbretasprata@gmail.com>
2019-08-21 02:55:41 -0300

    goit organized data for the graph

--- src/js/components/lineGraph.js
+++ src/js/components/lineGraph.js
@@ -9,7 +9,22 @@ class LineGraph extends Component {
         super();
         this.state = {
             failed: true,
-            messageError: ''
+            messageError: '',
+            months: {
+                1: "Jan",
+                2: "Fev",
+                3: "Mar",
+                4: "Apr",
+                5: "May",
+                6: "Jun",
+                7: "Jul",
+                8: "Ago",
+                9: "Sept",
+                10: "Oct",
+                11: "Nov",
+                12: "Dec"
+            }              
+            
         }
     }
     chartRef = React.createRef();
@@ -30,7 +45,7 @@ class LineGraph extends Component {
     
             const qtd = await Axios.get("http://localhost:2000/api/tickets/getQuantity", config);
     
-            const checked = checkError(qtd);
+            const checked = checkError(qtd);            
     
             if (checked.code) {
                 this.setState({
@@ -38,22 +53,69 @@ class LineGraph extends Component {
                     messageError: checked.message
                 });
             }
-    
-            return checked;
-
+            
+            const quantity = await this.setGraph(checked.result);
+            console.log(quantity);
+            return quantity;
 
         } catch (error) {
             this.setState({
                 failed: true,
                 messageError: error.message
             })
+        }       
+    }
+
+    setGraph = (tickets) => {
+        let month = new Date().getMonth() + 1;
+        let MonthValues = {};
+        let arrMonthValues = [];
+        let arrMonth = ["Jan", "Feb", "March", "Apr", "May", "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dec"];
+        let arrMonthNum = [];
+        let arrMonthsSorted = [];
+        let ct = 12;
+
+        tickets.forEach(e => {
+            MonthValues[e._id.month] = { 'result': e.result }
+        });
+
+        while(ct >= 1) {  
+
+            if(!MonthValues[month]) {
+                arrMonthValues.push(0);
+            } else {
+                arrMonthValues.push(MonthValues[month]['result']);
+            }
+
+            if(month == 1) {
+                arrMonthNum.push(month);
+                month = 12;
+            } else {
+                arrMonthNum.push(month);
+                month--;
+            }
+            ct--;
+        } 
+
+        for(let i = 0; i < 12; i++) {
+            month = arrMonth[arrMonthNum[i] - 1]
+            if(month) {
+                arrMonthsSorted.push(month);
+            }
         }
 
-        
+        let firstMonth = arrMonthsSorted.pop();
+        arrMonthsSorted.unshift(firstMonth);
+
+        return {
+            arrMonthValues: arrMonthValues.reverse(),
+            arrMonthsSorted: arrMonthsSorted.reverse()
+        }
     }
     
     componentDidMount() {
         const quantity = this.getQuantity();
+        console.log(quantity)
 
         const myChartRef = this.chartRef.current.getContext("2d");
         
@@ -61,11 +123,11 @@ class LineGraph extends Component {
             type: "line",
             data: {
                 //Bring in data
-                labels: ["Jan", "Feb", "March", "Apr", "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dec"],
+                labels: quantity.arrMonthsSorted,
                 datasets: [
                     {
                         label: "Tickets Completed",
-                        data: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                        data: quantity.arrMonthValues,
                     }
                 ]
             },
